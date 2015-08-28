e29652320070389f406496bae9fec9178ee35b52
zhc217 <zhc217@lehigh.edu>
2015-08-28 14:05:14 +0000

    updated HellowWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,18 @@
+//zhiyi Chen
+//Aug 28
+//define a class
+public class HelloWorld{
+    //add main method
+    public static void main(String[] args){
+        //print the statement with box around it
+        System.out.println("###########################");
+        System.out.println("###########################");
+        System.out.println("#                         #");
+        System.out.println("#   Hello World 2015!!    #");
+        System.out.println("#                         #");
+        System.out.println("###########################");
+        System.out.println("###########################");
+        System.out.println("test");
+        //test/testtest
+}
+}
