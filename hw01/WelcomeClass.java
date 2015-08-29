b5e4b7a761788270763725d0dd548a083fa72dc3
zhc217 <zhc217@lehigh.edu>
2015-08-29 18:18:52 +0000

    Updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,21 @@
+// Zhiyi Chen
+// Aug 29
+//CSE002 HW1
+//define a class
+public class WelcomeClass {
+//add a main method 
+public static void main(String[] args) {
+    //print the statement
+   System.out.println("    -----------     ");
+   System.out.println("    | WELCOME |     ");
+   System.out.println("    -----------     ");
+   System.out.println(" ^  ^  ^  ^  ^  ^   ");
+   System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\  ");
+   System.out.println("|-Z--H--C--2--1--7-|");
+   System.out.println("\\ /\\ /\\ /\\ /\\ /\\ /  ");
+   System.out.println(" v  v  v  v  v  v   ");
+   System.out.println("This Augest my friend and I took a one day, 100-mile");
+   System.out.println("voyage on our seagoing boat. We circulated San Franciso Bay.");
+// This concludes program.
+}
+}
