9daa5ba0d8ce9953f2913f781b002840ea309a34
zhc217 <zhc217@lehigh.edu>
2015-09-25 14:05:49 +0000

    updated CalculatorSpecial

---
+++ lab05/CalculatorSpecial.java
@@ -0,0 +1,64 @@
+// Zhiyi Chen
+//lab05
+//Sept 25
+
+import java.util.Scanner;
+
+public class CalculatorSpecial { // define a public class
+    public static void main(String [] args) { // define a main method
+    
+        Scanner myScanner = new Scanner( System.in);//declare an instance
+        
+        boolean e = true;// initialize it's true
+        while (e == true) { // while it's true, doing the calculation
+       
+       
+        System.out.print("Enter an integer a"); //prompt user to imput a
+        
+        int a = myScanner.nextInt();//call method nextDouble
+        
+        
+        System.out.print("Enter another inteter b"); //prompt user to imput b
+        
+        int b = myScanner.nextInt(); //call method nextDouble
+        
+        
+        System.out.print("Enter an operation(*,/,+,-)"); //prompt user to imput calculation method
+        
+        String sign = myScanner.next(); //call method nextChar
+        
+        String n, N,i;// define data type
+        
+       
+        
+       switch (sign) { //call method Swith
+           
+           case "*": System.out.println("output is" + (a * b) ); //if hits *, print out multipulation result
+           break; //return to buttom
+           
+           case "/": System.out.println("output is" + (a / b) ); //if hits /, print out divid result
+           break; //return to buttom
+           
+           case "+": System.out.println("output is" + (a + b) ); //if hits +, print out addition result
+           break; //return to buttom
+           
+           case "-": System.out.println("output is" + (a - b) ); //if hits -, print out subtraction result
+           break; //return to buttom
+           
+           default: System.out.println("ilegal");// if not other, print out illegal
+           break; //return to buttom
+       }
+       
+       System.out.println("Type n or N to stop");
+       
+       i = myScanner.next();//call method next()
+       
+       if ( i.equals("n") || i.equals("N") ) {
+           
+            e = false;//turns false if n or N is imputed
+       
+        }
+        }
+    }
+}
+        
